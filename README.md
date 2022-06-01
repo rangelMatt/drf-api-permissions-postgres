0f3063a3ca36fd5c1c6d729250fb06d3835e14ab
Matt Rangel <mattrangel@gmail.com>
2022-06-01 13:35:11 -0700

    added notes to readme

--- README.md
+++ README.md
@@ -24,6 +24,7 @@ To deploy locally:
 - `docker-compose up --build`
 - `docker-compose run web python manage.py migrate`
 - `docker-compose run web bash`
+  - `exit`
 
 ### Tests
 
--- sounds/tests.py
+++ sounds/tests.py
@@ -1,78 +1,78 @@
-# from django.contrib.auth import get_user_model
-# from django.urls import reverse
-# from rest_framework import status
-# from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# from .models import Thing
+from .models import Sound
 
 
-# class ThingTests(APITestCase):
-#     @classmethod
-#     def setUpTestData(cls):
-#         testuser1 = get_user_model().objects.create_user(
-#             username="testuser1", password="pass"
-#         )
-#         testuser1.save()
+class SoundTests(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        testuser1 = get_user_model().objects.create_user(
+            username="testuser1", password="pass"
+        )
+        testuser1.save()
 
-#         test_thing = Thing.objects.create(
-#             name="rake",
-#             owner=testuser1,
-#             description="Better for collecting leaves than a shovel.",
-#         )
-#         test_thing.save()
+        test_sound = Sound.objects.create(
+            name="rake",
+            owner=testuser1,
+            description="Better for collecting leaves than a shovel.",
+        )
+        test_sound.save()
 
-#     def test_things_model(self):
-#         thing = Thing.objects.get(id=1)
-#         actual_owner = str(thing.owner)
-#         actual_name = str(thing.name)
-#         actual_description = str(thing.description)
-#         self.assertEqual(actual_owner, "testuser1")
-#         self.assertEqual(actual_name, "rake")
-#         self.assertEqual(
-#             actual_description, "Better for collecting leaves than a shovel."
-#         )
+    def test_sounds_model(self):
+        sound = Sound.objects.get(id=1)
+        actual_owner = str(sound.owner)
+        actual_name = str(sound.name)
+        actual_description = str(sound.description)
+        self.assertEqual(actual_owner, "testuser1")
+        self.assertEqual(actual_name, "rake")
+        self.assertEqual(
+            actual_description, "Better for collecting leaves than a shovel."
+        )
 
-#     def test_get_thing_list(self):
-#         url = reverse("thing_list")
-#         response = self.client.get(url)
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         things = response.data
-#         self.assertEqual(len(things), 1)
-#         self.assertEqual(things[0]["name"], "rake")
+    def test_get_sound_list(self):
+        url = reverse("sound_list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        sounds = response.data
+        self.assertEqual(len(sounds), 1)
+        self.assertEqual(sounds[0]["name"], "rake")
 
-#     def test_get_thing_by_id(self):
-#         url = reverse("thing_detail", args=(1,))
-#         response = self.client.get(url)
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         thing = response.data
-#         self.assertEqual(thing["name"], "rake")
+    def test_get_sound_by_id(self):
+        url = reverse("sound_detail", args=(1,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        sound = response.data
+        self.assertEqual(sound["name"], "rake")
 
-#     def test_create_thing(self):
-#         url = reverse("thing_list")
-#         data = {"owner": 1, "name": "spoon", "description": "good for cereal and soup"}
-#         response = self.client.post(url, data)
-#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-#         things = Thing.objects.all()
-#         self.assertEqual(len(things), 2)
-#         self.assertEqual(Thing.objects.get(id=2).name, "spoon")
+    def test_create_sound(self):
+        url = reverse("sound_list")
+        data = {"owner": 1, "name": "spoon", "description": "good for cereal and soup"}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        sounds = Sound.objects.all()
+        self.assertEqual(len(sounds), 2)
+        self.assertEqual(Sound.objects.get(id=2).name, "spoon")
 
-#     def test_update_thing(self):
-#         url = reverse("thing_detail", args=(1,))
-#         data = {
-#             "owner": 1,
-#             "name": "rake",
-#             "description": "pole with a crossbar toothed like a comb.",
-#         }
-#         response = self.client.put(url, data)
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         thing = Thing.objects.get(id=1)
-#         self.assertEqual(thing.name, data["name"])
-#         self.assertEqual(thing.owner.id, data["owner"])
-#         self.assertEqual(thing.description, data["description"])
+    def test_update_sound(self):
+        url = reverse("sound_detail", args=(1,))
+        data = {
+            "owner": 1,
+            "name": "rake",
+            "description": "pole with a crossbar toothed like a comb.",
+        }
+        response = self.client.put(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        sound = Sound.objects.get(id=1)
+        self.assertEqual(sound.name, data["name"])
+        self.assertEqual(sound.owner.id, data["owner"])
+        self.assertEqual(sound.description, data["description"])
 
-#     def test_delete_thing(self):
-#         url = reverse("thing_detail", args=(1,))
-#         response = self.client.delete(url)
-#         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-#         things = Thing.objects.all()
-#         self.assertEqual(len(things), 0)
+    def test_delete_sound(self):
+        url = reverse("sound_detail", args=(1,))
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        sounds = Sound.objects.all()
+        self.assertEqual(len(sounds), 0)
